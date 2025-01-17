2f68c956874f35b7e97123829517150ec77e3923
elin-sunhye <tjsgp43@gmail.com>
2025-01-17 17:45:27 +0900

    backend 요청

--- korit_gpt_react/src/index.js
+++ korit_gpt_react/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import App12 from './study/App12';
+import App13 from './study/App13';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(<App />);
-root.render(<App12 />);
+root.render(<App13 />);
